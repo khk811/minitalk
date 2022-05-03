91d616bfe198fe944c762f89f71df730545090f9
khk811 <hyunkyungkim811@gmail.com>
2022-05-03 19:40:24 +0900

    write yuhwang_tuto

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,16 +6,56 @@
 /*   By: hyunkkim <hyunkkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 18:44:16 by hyunkkim          #+#    #+#             */
-/*   Updated: 2022/05/03 19:04:16 by hyunkkim         ###   ########seoul.kr  */
+/*   Updated: 2022/05/03 19:34:07 by hyunkkim         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	print_error(void)
+{
+	ft_printf("Error\n");
+	ft_printf("too few argument: need pid and string\n");
+	exit(1);
+}
+
+void	send_bit(int pid, int bit)
+{
+	if (!bit)
+	{
+		kill(pid, SIGUSR1);
+		usleep(150);
+	}
+	else
+	{
+		kill(pid, SIGUSR2);
+		usleep(150);
+	}
+}
+
+void	send_char(int pid, char c)
+{
+	send_bit(pid, c & 1<<7);
+	send_bit(pid, c & 1<<6);
+	send_bit(pid, c & 1<<5);
+	send_bit(pid, c & 1<<4);
+	send_bit(pid, c & 1<<3);
+	send_bit(pid, c & 1<<2);
+	send_bit(pid, c & 1<<1);
+	send_bit(pid, c & 1<<0);
+}
+
 int	main(int argc, char **argv)
 {
-	ft_printf("argc => %d\n", argc);
-	ft_printf("-> input pid: %s\n", argv[1]);
-	ft_printf("-> input string: %s\n", argv[2]);
+	int		server_pid;
+	char	*target_str;
+
+	if (argc < 3)
+		print_error();
+	server_pid = ft_atoi(argv[1]);
+	target_str = argv[2];
+	ft_printf("target-> %s\n", target_str);
+	while (*target_str)
+		send_char(server_pid, *(target_str++));
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,17 +6,44 @@
 /*   By: hyunkkim <hyunkkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 18:44:14 by hyunkkim          #+#    #+#             */
-/*   Updated: 2022/05/03 19:03:00 by hyunkkim         ###   ########seoul.kr  */
+/*   Updated: 2022/05/03 19:39:13 by hyunkkim         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	handler(int n)
+{
+	static char	input = 0;
+	static int	index = 0;
+
+	if (n == SIGUSR2)
+	{
+		input <<= 1;
+		input++;
+		index++;
+	}
+	else
+	{
+		input <<= 1;
+		index++;
+	}
+	if (index >= 8)
+	{
+		ft_printf("%c", input);
+		input = 0;
+		index = 0;
+	}
+}
+
 int	main(void)
 {
 	int	pid;
 
+	signal(SIGUSR1, &handler);
+	signal(SIGUSR2, &handler);
 	pid = getpid();
 	ft_printf("PID: %d\n", pid);
-	return (0);
+	while (1)
+		pause();
 }
