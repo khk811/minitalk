0af7090ec0c99ed39acc4e2da4eddbd8657466d2
khk811 <hyunkyungkim811@gmail.com>
2022-05-10 14:54:44 +0900

    check pid is valid or not

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 18:44:16 by hyunkkim          #+#    #+#             */
-/*   Updated: 2022/05/10 14:45:22 by hyunkkim         ###   ########seoul.kr  */
+/*   Updated: 2022/05/10 14:53:11 by hyunkkim         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@ void	print_error(void)
 	ft_printf("Error\n");
 	ft_printf("too few argument: need pid and string\n");
 	exit(1);
+}
+
+int	is_str_valid_pid(char *s)
+{
+	char	*tmp;
+	int		pid;
+
+	tmp = s;
+	while (*tmp)
+	{
+		if (!ft_isdigit(*tmp))
+			print_error();
+		tmp++;
+	}
+	pid = ft_atoi(s);
+	if (pid < 100 || pid > 99998)
+		print_error();
+	return (pid);
 }
 
 void	send_bit(int pid, int bit)
@@ -44,9 +62,8 @@ int	main(int argc, char **argv)
 
 	if (argc < 3)
 		print_error();
-	server_pid = ft_atoi(argv[1]);
+	server_pid = is_str_valid_pid(argv[1]);
 	target_str = argv[2];
-	ft_printf("target-> %s\n", target_str);
 	while (*target_str)
 		send_char(server_pid, *(target_str++));
 	return (0);
